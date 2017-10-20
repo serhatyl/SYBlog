9377de2fb1e91e9f61dcad21af688c7ed7925287
Angular CLI <angular-cli@angular.io>
2017-10-20 14:08:15 +0300

    chore: initial commit from @angular/cli

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule }   from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+RouterModule.forRoot([
+  {
+    path: '/',
+    component: AppComponent
+  }
+])
